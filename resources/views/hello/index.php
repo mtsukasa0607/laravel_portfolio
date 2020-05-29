af5e106956d860f4e27315776e1a272757b5c392
EC2 Default User <ec2-user@ip-172-31-34-226.ap-northeast-1.compute.internal>
2020-05-29 19:18:28 +0000

    Add post feature

---
+++ resources/views/hello/index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Hello/Index</title>
+    </head>
+    <body>
+        <h1>Hello</h1>
+        
+        <p>This is hello page with php-template.</p>
+    </body>
+</html>
