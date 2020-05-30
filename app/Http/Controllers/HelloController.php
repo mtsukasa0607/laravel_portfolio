7b975e9b5c351c23f1ef7063a47df38f2f515156
EC2 Default User <ec2-user@ip-172-31-0-182.ap-northeast-1.compute.internal>
2020-05-30 20:09:50 +0000

    suspended

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,23 +8,10 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
     // 検証コード
-    public function index(Request $request, Response $response) {
-        $html = <<< EOF
-            <html>
-                <head>
-                    <title>Hello/Index</title>
-                </head>
-                <body>
-                    <h1>Hello</h1>
-                    <h3>Request</h3>
-                    <pre>{$request}</pre>
-                    <h3>Response</h3>
-                    <pre>{$response}</pre>
-                    <p>This is hello page.</p>
-                </body>
-            </html>
-        EOF;
-        $response->setContent($html);
-        return $response;
+    public function index() {
+        $data = [
+            'msg'=>'これはBladeを利用したサンプルです。',
+        ];
+        return view('hello.index', $data);
     }
 }
