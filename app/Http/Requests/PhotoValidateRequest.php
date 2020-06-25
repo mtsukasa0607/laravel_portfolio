ba8b5e5ec6892e3249f21b6885e89e209f3e8c48
mtsukasa0607 <m.tsukasa0607@gmail.com>
2020-06-25 09:31:04 +0000

    Fixed PhotoValidateRequest

--- app/Http/Requests/PhotoValidateRequest.php
+++ app/Http/Requests/PhotoValidateRequest.php
@@ -6,27 +6,17 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class PhotoValidateRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
             'title' => 'required|max:20',
             'content' => 'required|max:400',
-            'file' => 'required',
+            'file' => 'required|image|mimes:jpeg,png,jpg,gif|max:10240|',
         ];
     }
 
@@ -34,9 +24,13 @@ class PhotoValidateRequest extends FormRequest
     {
         return [
             'title.required' => 'タイトルは必ず入力して下さい。',
+            'title.max' => 'タイトルは20字以内です。',
             'content.required' => 'コンテンツは必ず入力して下さい。',
+            'content.max' => 'コンテンツは400字以内です。',
             'file.required' => '画像は必ず選択して下さい。',
+            'file.image' => '指定されたファイルが画像ではありません。',
+            'file.mimes' => '指定された拡張子（PNG/JPG/JPEG/GIF）ではありません。',
+            'file.max' => 'サイズが10MBを超えています。',
         ];
     }
-
 }
