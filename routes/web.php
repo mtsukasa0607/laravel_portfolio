4d2845870a2163318138ecd594e4ac39a7009a87
mtsukasa0607 <m.tsukasa0607@gmail.com>
2020-06-25 10:58:04 +0000

    Remove Unnecessary Routing

--- routes/web.php
+++ routes/web.php
@@ -19,33 +19,6 @@ Route::get('/photo/photoDetail/{id?}', 'PhotoController@photoDetail');
 Route::post('/photo/photoDetail', 'PhotoController@photoComment')->middleware('auth');
 Route::get('/photo/photoCommentRemove/{id?}/{photo_id?}', 'PhotoController@photoCommentRemove');
 
-
-
-
-
-
-Route::get('/hello', 'HelloController@index');
-
-Route::get('/hello/messageShow', 'HelloController@messageShow');
-Route::post('/hello/messageShow', 'HelloController@messageCreate')->middleware('auth');
-Route::get('/hello/messageDelete', 'HelloController@messageDelete');
-Route::post('/hello/messageDelete', 'HelloController@messageRemove');
-Route::get('/hello/messageEdit', 'HelloController@messageEdit');
-Route::post('/hello/messageEdit', 'HelloController@messageUpdate');
-Route::get('/hello/post', 'HelloController@post');
-Route::post('/hello/post', 'HelloController@create');
-Route::get('/hello/check', 'HelloController@check');
-Route::get('/hello/show', 'HelloController@show');
-Route::get('/hello/delete', 'HelloController@delete');
-Route::get('/hello/storage_index', 'HelloController@storage_index');
-Route::get('/hello/other/{msg}', 'HelloController@other');
-Route::get('/hello/auth', 'HelloController@getAuth');
-Route::post('/hello/auth', 'HelloController@postAuth');
-Route::get('/hello/session', 'HelloController@ses_get');
-Route::post('/hello/session', 'HelloController@ses_put');
-
-
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
