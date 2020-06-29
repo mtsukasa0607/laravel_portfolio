73fd1b0be13f381f01c7b85a7f4cde5ebb97b2ad
mtsukasa0607 <m.tsukasa0607@gmail.com>
2020-06-29 16:10:08 +0000

    Add integration test code

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,18 +4,33 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\User;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $this->get('/photo/photoShow')->assertOk();
+        $this->get('/photo/photoFind')->assertOk();
 
-        $response->assertStatus(200);
+        $this->get('/photo/photoDetail/48')->assertOk();
+        // $this->get('/photo/photoAdd')->assertOk();
+
+
+
+        // $this->get('/home')->assertOk();
+        // $this->get('/photo/photoEdit/48')->assertOk();
+        // $response = $this->get('/');
+        // $response->assertStatus(200);
+        // $this->get('/hoge')->assertStatus(404);
+        // $this->get('/photo/photoShow')->assertOk();
     }
+
+    public function testLogout(): void
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->get('/photo/photoAdd')->assertStatus(200);
+
+    }
+
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -6,13 +6,12 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
     public function testBasicTest()
     {
         $this->assertTrue(true);
+        // $this->get('/photo/photoShow')->assertOk();
+        // $this->get('/hoge')->assertStatus(404);
+        // $this->visit('/hoge');
+        // $this->get('/photo/photoShow')->assertStatus(200);
     }
 }
