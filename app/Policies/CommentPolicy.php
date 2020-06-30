eb2645641d5a4eb02326bf4136d15acb12b26242
mtsukasa0607 <m.tsukasa0607@gmail.com>
2020-06-30 18:12:55 +0000

    Fixed policy

--- app/Policies/CommentPolicy.php
+++ app/Policies/CommentPolicy.php
@@ -10,47 +10,21 @@ class CommentPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
     public function viewAny(User $user)
     {
         //
     }
 
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Comment  $comment
-     * @return mixed
-     */
     public function view(User $user, Comment $comment)
     {
         //
     }
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
     public function create(User $user)
     {
         
     }
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Comment  $comment
-     * @return mixed
-     */
     public function update(User $user, Comment $comment)
     {
         //
@@ -61,25 +35,11 @@ class CommentPolicy
         return $user->id === $comment->user_id;
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Comment  $comment
-     * @return mixed
-     */
     public function restore(User $user, Comment $comment)
     {
         //
     }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Comment  $comment
-     * @return mixed
-     */
     public function forceDelete(User $user, Comment $comment)
     {
         //
--- app/Policies/PhotoPolicy.php
+++ app/Policies/PhotoPolicy.php
@@ -10,41 +10,20 @@ class PhotoPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
     public function viewAny(User $user)
     {
         //
     }
 
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Photo  $photo
-     * @return mixed
-     */
     public function view(User $user, Photo $photo)
     {
         //
     }
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
     public function create(User $user)
     {
         //
     }
-
-    
 
     public function update(User $user, Photo $photo)
     {
@@ -56,27 +35,11 @@ class PhotoPolicy
         return $user->id === $photo->user_id;
     }
 
-
-    
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Photo  $photo
-     * @return mixed
-     */
     public function restore(User $user, Photo $photo)
     {
         //
     }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Photo  $photo
-     * @return mixed
-     */
     public function forceDelete(User $user, Photo $photo)
     {
         //
