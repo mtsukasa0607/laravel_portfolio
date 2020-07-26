65060e5e68cddf01d2980d5e1b8acde1928907fb
mtsukasa0607 <m.tsukasa0607@gmail.com>
2020-07-26 13:46:50 +0000

    Add factory

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(App\Bookdetail::class, function (Faker $faker) {
+    $now = \Carbon\Carbon::now();
+    return [
+        'isbn' => $faker->isbn13,
+        'published_date' => $faker->date($format = 'Y-m-d', $max = 'now'),
+        'price' => $faker->randomNumber(4),
+        'created_at' => $now,
+        'updated_at' => $now,
+    ];
+});
